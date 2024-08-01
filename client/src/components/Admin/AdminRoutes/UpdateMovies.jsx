89cb8413409fe31acff7d499aafa96a926a07cc8
Subhajit Ghosh <subhajitghosh343@gmail.com>
2024-08-01 09:33:25 +0530

    update admin uiloader

--- client/src/components/Admin/AdminRoutes/UpdateMovies.jsx
+++ client/src/components/Admin/AdminRoutes/UpdateMovies.jsx
@@ -2,18 +2,23 @@ import React, { useEffect, useState } from "react";
 import UpdateCards from "./adminComponents/UpdateCards";
 import axios from "axios";
 import UpdateWrapper from "./adminComponents/UpdateWrapper";
+import {  FadeLoader } from "react-spinners";
 
 function UpdateMovies({ setisBackActive, toggle, settoggle }) {
   const [allMovies, setallMovies] = useState([]);
   const [toggleDelete, settoggleDelete] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function fetchAllMovies() {
+      setIsLoading(true);
       try {
         const res = await axios.get("http://localhost:5000/getallmovies");
         setallMovies(res.data.allMovies);
       } catch (error) {
         console.log("Error while fetching currentmoves", error);
+      } finally {
+        setIsLoading(false);
       }
     }
     fetchAllMovies();
@@ -25,16 +30,22 @@ function UpdateMovies({ setisBackActive, toggle, settoggle }) {
         <UpdateWrapper setisBackActive={setisBackActive} />
       ) : (
         <div className="flex flex-wrap gap-[2rem] w-full justify-center py-[3%]">
-          {allMovies.map((item, index) => {
-            return (
-              <UpdateCards
-                key={index}
-                item={item}
-                settoggle={settoggle}
-                settoggleDelete={settoggleDelete}
-              />
-            );
-          })}
+          {isLoading ? (
+            <div className="h-full w-full flex items-center justify-center">
+              <FadeLoader color="#ffffff" />
+            </div>
+          ) : (
+            allMovies.map((item, index) => {
+              return (
+                <UpdateCards
+                  key={index}
+                  item={item}
+                  settoggle={settoggle}
+                  settoggleDelete={settoggleDelete}
+                />
+              );
+            })
+          )}
         </div>
       )}
     </div>
